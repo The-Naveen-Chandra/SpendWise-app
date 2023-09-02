7a5920e2ddd5dbef3a2aa6520279429d8d194551
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-09-03 00:20:20 +0530

    Implemented dynamic bar graph according to the week day

--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spendwise_app/bargraph/custom_bar_graph.dart';
 import 'package:spendwise_app/data/expense_data.dart';
+import 'package:spendwise_app/datetime/date_time_helper.dart';
 
 class ExpenseSummary extends StatelessWidget {
   final DateTime startOfWeek;
@@ -13,18 +14,34 @@ class ExpenseSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // get yyyy mm dd for each of this weak
+    String sunday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 0)));
+    String monday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 1)));
+    String tuesday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 2)));
+    String wednesday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 3)));
+    String thursday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 4)));
+    String firday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 5)));
+    String saturday =
+        convertDateTimeToString(startOfWeek.add(const Duration(days: 6)));
+
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
         height: 200,
         child: CustomBarGraph(
           maxY: 100,
-          sunAmount: 20,
-          monAmount: 50,
-          tueAmount: 10,
-          wedAmount: 30,
-          thurAmount: 40,
-          firAmount: 3,
-          satAmount: 90,
+          sunAmount: value.calculateDailyExpenseSummary()[sunday] ?? 0,
+          monAmount: value.calculateDailyExpenseSummary()[monday] ?? 0,
+          tueAmount: value.calculateDailyExpenseSummary()[tuesday] ?? 0,
+          wedAmount: value.calculateDailyExpenseSummary()[wednesday] ?? 0,
+          thurAmount: value.calculateDailyExpenseSummary()[thursday] ?? 0,
+          firAmount: value.calculateDailyExpenseSummary()[firday] ?? 0,
+          satAmount: value.calculateDailyExpenseSummary()[saturday] ?? 0,
         ),
       ),
     );
